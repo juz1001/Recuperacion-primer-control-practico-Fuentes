bc51fbe07d265780c40428f2af6ac060b0c11d2f
JuliaUson <80346647+juz1001@users.noreply.github.com>
2021-12-21 21:59:18 +0100

    Intro

---
+++ Scrip.R
@@ -0,0 +1,14 @@
+---
+title: "Recuperación primer parcial Fuentes de Datos."
+autor: "Julia Usón Zuazo"
+date: "Diciembre 2021"
+
+output:
+  html_document: default
+pdf_document: default
+
+---
+## Introduccion
+  
+En este proyecto vamos a trabajar los contenidos que entraban en el primer parcial de prácticas de la asignatura trabajaremos
+los verbos de `dplyr`. Todos los verbos los voy a probar con el paquete starwars. Para realizar esto seguiremos los manuales "Vignettes".
